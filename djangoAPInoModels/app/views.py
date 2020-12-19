64ad9d15e8989ec55ed9df2fda28d6a18a861ac8
Ashish Sasmal <ashishsasmal1@gmail.com>
2020-12-19 16:22:31 +0530

    lecture 5

--- djangoAPInoModels/app/views.py
+++ djangoAPInoModels/app/views.py
@@ -6,19 +6,17 @@ from .models import *
 from django.views.generic import View
 from django.http import HttpResponse
 import json
+from django.core.serializers import serialize
 
 class EmployeeDetailsCBV(View):
     def get(self,request,*args,**kwargs):
         try:
-            data = Employee.objects.get(id=kwargs["id"])
-            resp = {
-                "Enum":data.enum,
-                "Ename":data.ename,
-                "Esal":data.esal,
-                "Eaddr":data.eaddr
-                }
-            resp = json.dumps(resp)
-            print(resp)
-            return HttpResponse(resp,content_type="application/json")
+            data = Employee.objects.filter(id=kwargs["id"])
+            
+            # select particular fields only to serialize using fields argument
+            # exclude will not in work here
+            json_data = serialize('json',data,fields=('enum','ename','eaddr'))
+            print(json_data)
+            return HttpResponse(json_data,content_type="application/json")
         except:
             return HttpResponse("<h1>Some Error Occured!</h1>")
--- djangoAPInoModels/test.py
+++ djangoAPInoModels/test.py
@@ -3,6 +3,9 @@ import requests
 BASE_URL = "http://127.0.0.1:8000/"
 END_POINT = "details/"
 
-def resource():
-    resp = requests.get(BASE_URL+END_POINT)
-    print()
+def resource(*args):
+    resp = requests.get(BASE_URL+END_POINT+str(args[0]))
+    print(resp.status_code)
+    print(resp.json())
+
+resource(3)
